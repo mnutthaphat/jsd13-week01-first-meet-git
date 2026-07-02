0085d55556f82b3da80c861a6f59fe2ef76c1556
Nutthaphat Mueangpool <m.nutthaphat@gmail.com>
2026-07-02 16:53:54 +0700

    edit

---
+++ my-object.js
@@ -0,0 +1,28 @@
+
+//let food = ["Pizza", "Malatang", "Pad thai", "apple"
+    
+
+let footDatabase = [ 
+    { id:1,
+        name: "Pizza",
+        cuisine: "Italian",
+        origin: " Italy ",
+        vegetarian: false,
+     },
+    { id:2,
+        name: "Malatang",
+        cuisine: "Chinese",
+        origin: " Various ",
+        vegetarian: true
+    },
+    { id:3,
+        name: "Pad thai",
+        cuisine: "Thai",
+        origin: " Thailand ",
+        vegetarian: false
+    },
+    { id:4,
+        name: "apple",
+        cuisine: "Fruit", }          
+            
+]   
